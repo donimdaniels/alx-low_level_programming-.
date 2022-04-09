d829d6a4f6143d3fbeb6e72a064dde2a3d6e8d98
donimdaniels <gamedesignsgds@gmail.com>
2022-04-09 06:01:17 -0700

    task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *my first, quote task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
+return (1);
+}
