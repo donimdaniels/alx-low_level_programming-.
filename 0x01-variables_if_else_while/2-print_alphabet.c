6218e1dd3e6e9a7f21325c58e475134a75131a3e
donimdaniels <gamedesignsgds@gmail.com>
2022-04-11 03:31:43 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic
+ *main - Prints the alphabetic
  *
- * Return: Always (Success)
+ *Return: Always (Success)
  */
 int main(void)
 {
@@ -12,6 +12,6 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
