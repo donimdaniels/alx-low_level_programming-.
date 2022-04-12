d152a28e2ef3e8fb1b12ba95b486f3fc865d8c10
donimdaniels <gamedesignsgds@gmail.com>
2022-04-12 07:18:05 -0700

    task update 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#inclde "main.h"
+#include "main.h"
 /**
- *main - entry block
- *Description: print -putchar in a new line
- *Return: 0
- */
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char ermias[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
