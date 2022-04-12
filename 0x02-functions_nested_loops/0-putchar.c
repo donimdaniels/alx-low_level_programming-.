d3adb1f3f25c846be812a931ccc0373da5ba627f
donimdaniels <gamedesignsgds@gmail.com>
2022-04-12 01:56:49 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#inclde "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+	char ermias[] - "_putchar";
+
+	int c;
+
+	for (c = 0; < 8; c++)
+	{
+		_putchar(ermias[c];
+	}
+	_putchar('\n');
+	return (0);
+}
